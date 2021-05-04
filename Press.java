3fce9216aea1f1afab1cf2999f9b2f0a0152c809
jash2002 <ashjonathan2002@gmail.com>
2021-05-04 13:55:35 +0100

    UniWork_Comp122_A3_Update

--- Press.java
+++ Press.java
@@ -1,28 +1,29 @@
 import java.nio.file.Path;
 import java.io.File;
-import java.io.FilenameFilter;
 import java.nio.file.*;
 import java.util.Map;
 import java.util.Queue;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.regex.*;
 
 
 public class Press {
 
     private int booksPerEdition;
-    private Map<String, Integer> edition;
+    private HashMap<String, Integer> edition;
     private Map<String, Queue<Book>> shelf;
 
     public Press(String p, int n) {
         booksPerEdition = n;
-        edition.put(p, booksPerEdition);
-        File file = new File("p");
-        File[] matchingFiles = file.listFiles(new FilenameFilter() {
-            public boolean accept(File dir, String name) {
-                return name.endsWith("txt");
-            }
-        });
+        File directoryPath = new File("p");
+        File filesList[] = directoryPath.listFiles();
+        for (File a: filesList) {
+            String b = a.getName();
+            edition.put(b, n);
+        }
+
     }
 
     public List<Book> print(String ID) {
