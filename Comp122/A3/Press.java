dc11fd506fa160d26dcf40be79bc90437a30a7ef
jash2002 <ashjonathan2002@gmail.com>
2021-05-04 21:57:51 +0100

    finishing up regex

--- Comp122/A3/Press.java
+++ Comp122/A3/Press.java
@@ -2,6 +2,7 @@
 import java.nio.file.Path;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.*;
 import java.util.Map;
 import java.util.Queue;
@@ -20,7 +21,7 @@ public class Press {
     private HashMap<String, Integer> edition;
     private Map<String, Queue<Book>> shelf;
 
-    public Press(String p, int n) {
+    public Press(String p, int n) throws IOException {
         booksPerEdition = n;
         File directoryPath = new File("p");
         File filesList[] = directoryPath.listFiles();
@@ -30,7 +31,7 @@ public class Press {
             edition.put(a.getName(), n);
         }        
             
-        HashMap<String, LinkedList<Book>> shelf = new HashMap<String,LinkedList<Book>>();
+        HashMap<String, LinkedList<Book>> shelf = new HashMap<String, LinkedList<Book>>();
 
 
         for (int i = 0; i < filesList.length; i++) {
@@ -43,16 +44,19 @@ public class Press {
             all = Files.readString(file.toPath());
             Pattern patternT = Pattern.compile("Title: (.*)");
             Matcher matcherT = patternT.matcher(all);
-            title = matcherT.toString();
+            title = matcherT.group(1);
 
             Pattern patternA = Pattern.compile("Author: (.*)");
             Matcher matcherA = patternA.matcher(all);
-            author = matcherA.toString();
+            author = matcherA.group(1);
 
-            Pattern patternC = Pattern.compile("");
+            Pattern patternC = Pattern.compile("(?<=(.\s/*/*/*)(?s)(.*$))");
             Matcher matcherC = patternC.matcher(all);
-            content = matcherC.toString();
-              
+            content = matcherC.group(1);
+
+            LinkedList<Book> books = new LinkedList<Book>();
+            books.add(new Book(title, author, content, i++));
+            
             
         }   
     }   
