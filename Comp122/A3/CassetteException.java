394a57680c34400908d9e38c19cea76021e65373
jash2002 <ashjonathan2002@gmail.com>
2021-05-04 15:08:41 +0100

    UniWork_Comp122_A3_Update

---
+++ Comp122/A3/CassetteException.java
@@ -0,0 +1,6 @@
+public class CassetteException extends Exception {
+
+    public CassetteException(String message) {
+        super(message);
+    }
+}
--- Comp122/A3/Press.java
+++ Comp122/A3/Press.java
@@ -1,13 +1,16 @@
 
 import java.nio.file.Path;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.file.*;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.regex.*;
+
 
 
 public class Press {
@@ -20,23 +23,38 @@ public class Press {
         booksPerEdition = n;
         File directoryPath = new File("p");
         File filesList[] = directoryPath.listFiles();
+        
+        HashMap<String, Integer> edition = new HashMap<String, Integer>();
         for (File a: filesList) {
-            String b = a.getName();
-            edition.put(b, n);
+            edition.put(a.getName(), n);
+        }        
+            
+            
+
+        //    Pattern patternC = Pattern.compile("", Pattern.CASE_INSENSITIVE);
+        //    Matcher matcherC = patternC.matcher(input);
+        //    String Content = matcherC.toString();
+
+        //    Pattern patternD = Pattern.compile("Author", Pattern.CASE_INSENSITIVE);
+        //    Matcher matcherA = patternA.matcher(input);
+        //   String Author = matcherA.toString();
+
+
         }
+    }    
 
-    }
+    //}
 
-    public List<Book> print(String ID) {
+   // public List<Book> print(String ID) {
 
-    }
+   // }
 
-    public List<String> getCatalogue() {
+   // public List<String> getCatalogue() {
 
-    }
+  //  }
 
-    public List<Book> request(String ID, int n) {
+   // public List<Book> request(String ID, int n) {
 
-    }
+  //  }
 
 }
